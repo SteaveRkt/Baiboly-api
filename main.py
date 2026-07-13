a238c5f018d498c1f5a0c6f64f6cc6819238bbb7
Rkt <steaverkt@gmail.com>
2026-07-13 22:21:30 +0300

    adding get book

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from fastapi import FastAPI, HTTPException
+from typing import Annotated,List
+from fastapi import FastAPI, HTTPException,Path
 from pathlib import Path
+from starlette import status
 import json
 
 app = FastAPI()
 
-
+baiboly=Path("baiboly-json")
 Testamenta_Taloha = Path("baiboly-json/Testameta taloha")
 Testamenta_Vaovao = Path("baiboly-json/Testameta vaovao")
 
-@app.get("/livres/{nom_livre}")
-async def lire_livre(nom_livre: str):
+#liste des livres
+@app.get("/livres",status_code=status.HTTP_200_OK)
+async def liste_livre():
+    data=[]
+    def parcours(dir):
+        for f in sorted(dir.iterdir()):
+            if f.is_dir():
+                parcours(f)
+            if f.is_file():
+                with open(f.absolute(),encoding="utf-8") as b:
+                    boky=json.load(b)
+                    anarana=boky["meta"]["name"]
+                dic={"id":(boky["meta"]["order"]),"titre":anarana,"abreviation":anarana[:3].upper(),"testameta":f.parent.stem.split()[1],"nombre_chapitre":boky["meta"]["chapter_number"]}
+                data.append(dic)
+    parcours(baiboly)
+    data=sorted(data,key=lambda x:x["id"])
+    return data
+
+
+
+
+
+#contenu d'un livre
+@app.get("/livres/{nom_livre}",status_code=status.HTTP_200_OK)
+async def contenu_livre(nom_livre:Annotated[str,Path()]):
     
     TT= Testamenta_Taloha / f"{nom_livre}.json"
     TV=Testamenta_Vaovao / f"{nom_livre}.json"

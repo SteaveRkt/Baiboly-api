6bb49e605845aad7c54f85e0db1e19194fc0de6b
Rkt <steaverkt@gmail.com>
2026-07-14 00:29:14 +0300

    refactor getting book by name

--- main.py
+++ main.py
@@ -32,25 +32,16 @@ async def liste_livre():
 #contenu d'un livre
 @app.get("/livres/{nom_livre}",status_code=status.HTTP_200_OK)
 async def contenu_livre(nom_livre:Annotated[str,Path()]):
-    def trouver_titre (dossier,nom):
-        for f in sorted(dossier.iterdir()):
-            if f.is_dir():
-                res=trouver_titre(f,nom)
-                if res is not False:
-                    return res
-            elif f.is_file():
-                with open (f.absolute(),encoding="utf-8") as l:
-                    livre=json.load(l)
-                    anarana=livre["meta"]["name"].lower().replace(" ","")
-                if nom in anarana :
-                    premier_chapitre =livre["1"]
-                    return premier_chapitre
-        return False
     nom_livre=nom_livre.lower().replace(" ","")
-    data=trouver_titre(baiboly,nom_livre)
-    if data==False:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Livre non trouvé")
-    return data
+    for f in baiboly.rglob("*.json"):
+        with open (f,encoding="utf-8") as l:
+            livre=json.load(l)
+            anarana=livre["meta"]["name"].lower().replace(" ","")
+        if nom_livre in anarana :
+            premier_chapitre =livre["1"]
+            return premier_chapitre
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Livre non trouvé")
+
                     
 
             
